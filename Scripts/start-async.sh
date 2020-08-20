3488a0f2995e698fce5999019d1b308419e3db1b
flapek <filap4@gmail.com>
2020-08-20 21:51:31 +0200

    update assembly info and csproj

--- Scripts/start-async.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export ASPNETCORE_ENVIRONMENT=local
-export NTRADA_CONFIG=ntrada-async
-cd src/Epilepsy_Health_App.APIGateway
-dotnet run
---
+++ src/Epilepsy_Health_App.APIGateway/Properties/Assembly.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Epilepsy_Health_App.Services.APIGateway")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Epilepsy_Health_App.Services.APIGateway")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5C2E9A9F-5A1E-4623-8137-C9616F8AE407")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+// Major Version
+// Minor Version
+// Build Number
+// Revision
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
