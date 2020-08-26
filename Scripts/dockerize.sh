1c638c492c8d85bd40c10a5621396c7f496a019c
flapek <filap4@gmail.com>
2020-08-26 22:29:16 +0200

    add travis file

---
+++ Scripts/dockerize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TAG=latest
+VERSION_TAG=$TRAVIS_BUILD_NUMBER
+
+REPOSITORY=$DOCKER_USERNAME/Epilepsy_Health_App.APIGateway
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
+docker push $REPOSITORY:$TAG
+docker push $REPOSITORY:$VERSION_TAG
